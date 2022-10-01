05c37aa0e6523a6c1ab17667b4f21e7c83ab72f4
NicolasMontone <montonenicolas01@gmail.com>
2022-10-01 14:50:00 -0300

    fix: add missing components in app.js.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Physics } from '@react-three/cannon';
+import { Sky } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import { FirstPersonView } from './components/FirstPersonView';
+import { Ground } from './components/Ground';
+import { Player } from './components/Player';
+
 
 function App() {
   return (
     <Canvas>
+      <Sky sunPosition={[100, 100, 20]} />
+      <ambientLight intensity={0.5} />
+      <FirstPersonView />
+      <Physics>
+        <Player />
+        <Ground />
+      </Physics>
     </Canvas>
   );
 }
